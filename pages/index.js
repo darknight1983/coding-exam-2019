1ac6a707d16f4adc7fb1965cb3cd6f2f81d12209
Jacolby Green <darknightnov@gmail.com>
2019-02-21 18:13:59 -0600

    React.Suspense

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import Header from "../components/header";
 import SearchBar from "../components/searchBar";
 import RepoList from "../components/repoList";
+import useFetch from "fetch-suspense";
 import { mainContainer } from "../components/sharedStyles";
 
 class App extends Component {
@@ -9,29 +10,30 @@ class App extends Component {
     repos: []
   };
   grabUserRepos = name => {
-    console.log(name);
-
-    // Be sure to validate for blank characters
-    if (name) {
-      fetch(`https://api.github.com/users/${name}/repos`)
-        .then(response => response.json())
-        .then(response => {
-          console.log(response[0]);
-          this.setState({
-            repos: response.filter(
-              repo => repo.fork !== true && repo.stargazers_count >= 1
-            )
-          });
-        })
-        .catch(error => console.error(error));
-    }
+    let data = useFetch(`https://api.github.com/users/${name}/repos`);
+    console.log(data);
+    // if (name) {
+    //   fetch(`https://api.github.com/users/${name}/repos`)
+    //     .then(response => response.json())
+    //     .then(response => {
+    //       console.log(response[0]);
+    //       this.setState({
+    //         repos: response.filter(
+    //           repo => repo.fork !== true && repo.stargazers_count >= 1
+    //         )
+    //       });
+    //     })
+    //     .catch(error => console.error(error));
+    // }
   };
   render() {
     return (
       <div style={mainContainer}>
         <Header />
         <SearchBar grabRepos={this.grabUserRepos} />
-        <RepoList repos={this.state.repos} />
+        <Suspense fallback="Loading...">
+          <RepoList repos={this.state.repos} />
+        </Suspense>
       </div>
     );
   }
