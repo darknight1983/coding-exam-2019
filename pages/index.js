9502dcb6c12ee10b50a5d924c484cd40aea25576
Jacolby Green <darknightnov@gmail.com>
2019-02-22 14:47:22 -0600

    added useState hook

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,35 @@
-import React, { Component } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import Header from "../components/header";
 import SearchBar from "../components/searchBar";
 import RepoList from "../components/repoList";
+
 import { mainContainer } from "../components/sharedStyles";
 
-class App extends Component {
-  state = {
-    repos: []
-  };
-  grabUserRepos = name => {
+const App = () => {
+  const [repos, setRepos] = useState([]);
+
+  const grabUserRepos = name => {
     if (name) {
       fetch(`https://api.github.com/users/${name}/repos`)
         .then(response => response.json())
         .then(response => {
-          console.log(response[0]);
-          this.setState({
-            repos: response.filter(
+          setRepos(
+            response.filter(
               repo => repo.fork !== true && repo.stargazers_count >= 1
             )
-          });
+          );
         })
         .catch(error => console.error(error));
     }
   };
-  render() {
-    return (
-      <div style={mainContainer}>
-        <Header />
-        <SearchBar grabRepos={this.grabUserRepos} />
-        <RepoList repos={this.state.repos} />
-      </div>
-    );
-  }
-}
+
+  return (
+    <div style={mainContainer}>
+      <Header />
+      <SearchBar grabRepos={grabUserRepos} />
+      <RepoList repos={repos} />
+    </div>
+  );
+};
 
 export default App;
