3f6ad4cb313b23039afc294c4b7a5eda445e2e79
Jacolby Green <darknightnov@gmail.com>
2019-02-21 22:37:37 -0600

    useState -> searchComp

--- components/searchBar.js
+++ components/searchBar.js
@@ -1,38 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { searchBar, searchBarContainer } from "./sharedStyles";
 
-class SearchBar extends Component {
-  state = {
-    name: " "
-  };
+const SearchBar = props => {
+  const [name, setName] = useState(" ");
 
-  handleChange = query => {
-    const { name, value } = query.target;
-    this.setState({
-      [name]: value.trim()
-    });
-  };
-  handleSubmit = e => {
+  function handleNameChange(e) {
+    setName(e.target.value.trim());
+    console.log(name);
+  }
+
+  const handleSubmit = e => {
     if (e.key === "Enter") {
-      this.props.grabRepos(this.state.name);
+      props.grabRepos(name);
       // Find a better way to clear the name property.
-      this.setState({ name: " " });
+      setName(" ");
     }
   };
-  render() {
-    const { name } = this.state;
-    return (
-      <div style={searchBarContainer}>
-        <input
-          value={name}
-          onChange={this.handleChange}
-          name="name"
-          onKeyUp={this.handleSubmit}
-          style={searchBar}
-        />
-      </div>
-    );
-  }
-}
+
+  return (
+    <div style={searchBarContainer}>
+      <input
+        value={name}
+        onChange={handleNameChange}
+        name="name"
+        onKeyUp={handleSubmit}
+        style={searchBar}
+      />
+    </div>
+  );
+};
 
 export default SearchBar;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import React, { Component, Suspense } from "react";
+import React, { Component } from "react";
 import Header from "../components/header";
 import SearchBar from "../components/searchBar";
 import RepoList from "../components/repoList";
-import useFetch from "fetch-suspense";
 import { mainContainer } from "../components/sharedStyles";
 
 class App extends Component {
@@ -10,30 +9,26 @@ class App extends Component {
     repos: []
   };
   grabUserRepos = name => {
-    let data = useFetch(`https://api.github.com/users/${name}/repos`);
-    console.log(data);
-    // if (name) {
-    //   fetch(`https://api.github.com/users/${name}/repos`)
-    //     .then(response => response.json())
-    //     .then(response => {
-    //       console.log(response[0]);
-    //       this.setState({
-    //         repos: response.filter(
-    //           repo => repo.fork !== true && repo.stargazers_count >= 1
-    //         )
-    //       });
-    //     })
-    //     .catch(error => console.error(error));
-    // }
+    if (name) {
+      fetch(`https://api.github.com/users/${name}/repos`)
+        .then(response => response.json())
+        .then(response => {
+          console.log(response[0]);
+          this.setState({
+            repos: response.filter(
+              repo => repo.fork !== true && repo.stargazers_count >= 1
+            )
+          });
+        })
+        .catch(error => console.error(error));
+    }
   };
   render() {
     return (
       <div style={mainContainer}>
         <Header />
         <SearchBar grabRepos={this.grabUserRepos} />
-        <Suspense fallback="Loading...">
-          <RepoList repos={this.state.repos} />
-        </Suspense>
+        <RepoList repos={this.state.repos} />
       </div>
     );
   }
