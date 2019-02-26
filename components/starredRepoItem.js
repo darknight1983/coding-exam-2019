82c0477f92e57c25e6ed4b07c0e878956e11d511
Jacolby Green <darknightnov@gmail.com>
2019-02-26 11:48:24 -0600

    link to gitUser page

--- components/starredRepoItem.js
+++ components/starredRepoItem.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 import { repoItem, h1, center, button } from "./sharedStyles";
 
 const StarredRepo = ({ name, language, description }) => {
   return (
-    <div style={repoItem}>
-      <h1 style={h1}>{name}</h1>
-      <hr />
-      <h3 style={button}>{language}</h3>
-      <p style={center}>{description}</p>
-    </div>
+    <Link href="/gitUser">
+      <div style={repoItem}>
+        <h1 style={h1}>{name}</h1>
+        <hr />
+        <h3 style={button}>{language}</h3>
+        <p style={center}>{description}</p>
+      </div>
+    </Link>
   );
 };
 
---
+++ pages/gitUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/header";
+import { mainContainer, header } from "../components/sharedStyles";
+
+const UserPage = () => {
+  return (
+    <div style={mainContainer}>
+      <Header />
+    </div>
+  );
+};
+export default UserPage;
